2c9ac2f2345357765da76e4abc335f5d33c87973
hseniht <thinesh_kumar_mu@hotmail.com>
2020-03-08 12:02:30 +0800

    Updates

---
+++ precache-manifest.95dfdd93c64676f75a30ece3254a36f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ebdeb76eca8ac0f7120de820811af9",
+    "url": "/sunday-react-1/index.html"
+  },
+  {
+    "revision": "917c7fba9252de3e2f0a",
+    "url": "/sunday-react-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "335088ada64a491e096c",
+    "url": "/sunday-react-1/static/js/2.b838b1d5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sunday-react-1/static/js/2.b838b1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917c7fba9252de3e2f0a",
+    "url": "/sunday-react-1/static/js/main.a32ae249.chunk.js"
+  },
+  {
+    "revision": "feee824ebb9081792d53",
+    "url": "/sunday-react-1/static/js/runtime-main.0df18dee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sunday-react-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
